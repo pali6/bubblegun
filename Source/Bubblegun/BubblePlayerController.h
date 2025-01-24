59f9e5d84caf09cb78d1f94218ce501b33e20184
Ermith <f.s.ermith@gmail.com>
2025-01-24 20:26:23 +0100

    New Classes and Reorganization

---
+++ Source/Bubblegun/BubblePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BubblePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUBBLEGUN_API ABubblePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
