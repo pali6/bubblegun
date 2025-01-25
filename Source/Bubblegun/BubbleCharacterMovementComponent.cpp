495f434a4b3c675d624c5f27bbb1213dca250813
Ermith <f.s.ermith@gmail.com>
2025-01-25 12:30:54 +0100

    Dash

--- Source/Bubblegun/BubbleCharacterMovementComponent.cpp
+++ Source/Bubblegun/BubbleCharacterMovementComponent.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Curves/CurveFloat.h"
 #include "BubbleCharacterMovementComponent.h"
+#include "Curves/CurveFloat.h"
 
 float UBubbleCharacterMovementComponent::GetGravityZ() const
 {
